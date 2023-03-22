631d98e9757b48ef17b7b3c69bfe0aa09c4381da
Jezer05 <jezer05@qq.com>
2023-03-22 22:42:31 +0800

    upgrade Song API

--- music-server/src/main/java/top/jezer/controller/SongController.java
+++ music-server/src/main/java/top/jezer/controller/SongController.java
@@ -1,13 +1,11 @@
 package top.jezer.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import top.jezer.common.ErrorResp;
 import top.jezer.common.SuccessResp;
-import top.jezer.common.WarningResp;
 import top.jezer.constant.ResourceLocation;
 import top.jezer.domain.Song;
 import top.jezer.exception.SystemException;
@@ -23,51 +21,33 @@ public class SongController {
     private SongService songService;
     // 添加新歌曲
     @PostMapping("/add")
-    public Object addSong(HttpServletRequest req, @RequestParam("file") MultipartFile mpfile) {
-        // 资源判重，数据判空
-        String singer_id = req.getParameter("singerId");
-        String name = req.getParameter("name");
-        String introduction = req.getParameter("introduction");
-        String pic = "/img/songPic/tubiao.jpg";
-        String lyric = req.getParameter("lyric");
-
-        Song song = new Song();
-        if (StringUtils.isNotBlank(singer_id) && StringUtils.isNotBlank(name)){
-            song.setSingerId(Integer.parseInt(singer_id.trim()));
-            song.setName(name.trim());
-        }else return new ErrorResp("歌手id和歌曲名称不能为空").getMessage();
-        if (StringUtils.isNotBlank(introduction))
-            song.setIntroduction(introduction.trim());
-        if (StringUtils.isNotBlank(lyric))
-            song.setLyric(lyric.trim());
-        song.setPic(pic);
-
-        try {
-            if (songService.getSongByNameEq(song.getName()) != null)
-                return new ErrorResp("已存在重名资源！").getMessage();
-        }catch (Exception e){
-            throw new SystemException();
-        }
-
-        String fileName = mpfile.getOriginalFilename();
+    public Object addSong(Song song, @RequestParam("file") MultipartFile file) {
+        //  禁止用户自定义ID
+        song.setId(null);
+        song.setPic("/img/songPic/tubiao.jpg");
+        // String会自动转成Integer
+        if (null == song.getSingerId() || StringUtils.isBlank(song.getName()))
+            return new ErrorResp("歌手id和歌曲名称不能为空").getMessage();
+        // 创建音乐资源路径
+        String fileName = file.getOriginalFilename();
         String filePath = System.getProperty("user.dir") + ResourceLocation.ASSETS_PATH + "/song";
         File file1 = new File(filePath);
         if (!file1.exists()) {
             file1.mkdir();
         }
-
         File dest = new File(filePath + System.getProperty("file.separator") + fileName);
         String storeUrlPath = "/song/" + fileName;
+        song.setUrl(storeUrlPath);
+        // TODO 查看是否存在同名歌曲
         try {
-            song.setUrl(storeUrlPath);
-            mpfile.transferTo(dest);
+            file.transferTo(dest);
             boolean res = songService.addSong(song);
             if (res) {
-                return new SuccessResp<>("上传成功", storeUrlPath).getMessage();
+                return new SuccessResp<>("歌曲添加成功").getMessage();
             } else {
-                return new ErrorResp("上传失败").getMessage();
+                return new ErrorResp("歌曲添加失败").getMessage();
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             throw new SystemException("系统繁忙，请稍后再试");
         }
     }
@@ -77,9 +57,9 @@ public class SongController {
         try{
             boolean res = songService.deleteSong(id);
             if (res) {
-                return new SuccessResp<>("删除成功").getMessage();
+                return new SuccessResp<>("歌曲删除成功").getMessage();
             } else {
-                return new ErrorResp("删除失败").getMessage();
+                return new ErrorResp("歌曲删除失败").getMessage();
             }
         }catch(Exception e){
             throw new SystemException("系统繁忙，请稍后再试");
@@ -89,7 +69,7 @@ public class SongController {
     @GetMapping
     public Object getAllSong(){
         try{
-            return new SuccessResp<>("查询成功", songService.getAllSong()).getMessage();
+            return new SuccessResp<>("歌曲查询成功", songService.getAllSong()).getMessage();
         }catch (Exception e){
             throw new SystemException("系统繁忙，请稍后再试");
         }
@@ -98,7 +78,8 @@ public class SongController {
     @GetMapping("/{id}")
     public Object getSongById(@PathVariable("id") Integer id){
         try{
-            return new SuccessResp<>("查询成功", songService.getSongById(id)).getMessage();
+            // 没有数据也是查询成功，只是data为null
+            return new SuccessResp<>("歌曲查询成功", songService.getSongById(id)).getMessage();
         }catch (Exception e){
             throw new SystemException("系统繁忙，请稍后再试");
         }
@@ -107,50 +88,31 @@ public class SongController {
     @GetMapping("/singer/detail/{id}")
     public Object getSongBySingerId(@PathVariable("id") Integer singerId){
         try{
-            return new SuccessResp<>("查询成功",songService.getSongBySingerId(singerId)).getMessage();
+            return new SuccessResp<>("歌曲查询成功",songService.getSongBySingerId(singerId)).getMessage();
         }catch (Exception e){
             throw new SystemException("系统繁忙，请稍后再试");
         }
     }
     // 根据歌手查询歌曲
     @GetMapping("/singerName/detail")
-    public Object getSongByName(HttpServletRequest req){
-        String name = req.getParameter("name");
-        if (StringUtils.isNotBlank(name))
-            name = name.trim();
+    public Object getSongByName(String name){
         try{
-            return new SuccessResp<>("查询成功", songService.getSongByName(name)).getMessage();
+            return new SuccessResp<>("歌曲查询成功", songService.getSongByName(name)).getMessage();
         }catch (Exception e){
             throw new SystemException("系统繁忙，请稍后再试");
         }
     }
     // 更新歌曲信息
     @PutMapping("/update")
-    public Object updateSongMsg(HttpServletRequest req) {
-        Song song = new Song();
-        String id = req.getParameter("id");
-        String singer_id = req.getParameter("singerId");
-        String name = req.getParameter("name");
-        String introduction = req.getParameter("introduction");
-        String lyric = req.getParameter("lyric");
-        if (StringUtils.isBlank(id))
-            return new WarningResp("id不能为空").getMessage();
-        else
-            song.setId(Integer.parseInt(id.trim()));
-        if (StringUtils.isNotBlank(singer_id))
-            song.setSingerId(Integer.parseInt(singer_id.trim()));
-        if (StringUtils.isNotBlank(name))
-            song.setName(name.trim());
-        if (StringUtils.isNotBlank(introduction))
-            song.setIntroduction(introduction.trim());
-        if (StringUtils.isNotBlank(lyric))
-            song.setLyric(lyric.trim());
+    public Object updateSongMsg(Song song) {
+        if (null == song.getId())
+            return new ErrorResp("歌曲id不能为空").getMessage();
         try {
             boolean res = songService.updateSong(song);
             if (res) {
-                return new SuccessResp<>("修改成功").getMessage();
+                return new SuccessResp<>("歌曲信息修改成功").getMessage();
             } else {
-                return new ErrorResp("修改失败").getMessage();
+                return new ErrorResp("歌曲信息修改失败").getMessage();
             }
         }catch (Exception e){
             throw new SystemException("系统繁忙，请稍后再试");
@@ -158,9 +120,9 @@ public class SongController {
     }
     // 更新歌曲图片
     @PutMapping("/updateImg/{id}")
-    public Object updateSongPic(@RequestParam("file") MultipartFile urlFile, @PathVariable("id") Integer id) {
+    public Object updateSongPic(@RequestParam("file") MultipartFile file, @PathVariable("id") Integer id) {
         // TODO 删除旧图片
-        String fileName = System.currentTimeMillis() + urlFile.getOriginalFilename();
+        String fileName = System.currentTimeMillis() + file.getOriginalFilename();
         String filePath = System.getProperty("user.dir") + ResourceLocation.ASSETS_PATH + "/img/songPic";
         File file1 = new File(filePath);
         if (!file1.exists()) {
@@ -169,15 +131,15 @@ public class SongController {
         File dest = new File(filePath + System.getProperty("file.separator") + fileName);
         String storeUrlPath = "/img/songPic/" + fileName;
         try {
-            urlFile.transferTo(dest);
+            file.transferTo(dest);
             Song song = new Song();
             song.setId(id);
             song.setPic(storeUrlPath);
             boolean res = songService.updateSong(song);
             if (res) {
-                return new SuccessResp<>("上传成功", storeUrlPath).getMessage();
+                return new SuccessResp<>("歌曲图片上传成功", storeUrlPath).getMessage();
             } else {
-                return new ErrorResp("上传失败").getMessage();
+                return new ErrorResp("歌曲图片上传失败").getMessage();
             }
         } catch (IOException e) {
             return new SystemException();
@@ -186,9 +148,9 @@ public class SongController {
 
     // 更新歌曲资源
     @PutMapping("/updateUrl/{id}")
-    public Object updateSongUrl(@RequestParam("file") MultipartFile urlFile, @PathVariable("id") Integer id) {
+    public Object updateSongUrl(@RequestParam("file") MultipartFile file, @PathVariable("id") Integer id) {
         // TODO 删除旧资源
-        String fileName = urlFile.getOriginalFilename();
+        String fileName = file.getOriginalFilename();
         String filePath = System.getProperty("user.dir") + ResourceLocation.ASSETS_PATH + "/song";
         File file1 = new File(filePath);
         if (!file1.exists()) {
@@ -197,7 +159,7 @@ public class SongController {
         File dest = new File(filePath + System.getProperty("file.separator") + fileName);
         String storeUrlPath = "/song/" + fileName;
         try {
-            urlFile.transferTo(dest);
+            file.transferTo(dest);
             Song song = new Song();
             song.setId(id);
             song.setUrl(storeUrlPath);
--- music-server/src/main/java/top/jezer/service/serviceImpl/SongServiceImpl.java
+++ music-server/src/main/java/top/jezer/service/serviceImpl/SongServiceImpl.java
@@ -1,6 +1,7 @@
 package top.jezer.service.serviceImpl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import top.jezer.domain.Song;
@@ -15,11 +16,46 @@ public class SongServiceImpl implements SongService {
     private SongMapper songMapper;
     @Override
     public boolean addSong(Song song) {
+        // 去除首尾空格，trim不适用于null
+        String name = song.getName();
+        String introduction = song.getIntroduction();
+        String lyric = song.getLyric();
+        if (null != name)
+            name = name.trim();
+        if (null != introduction)
+            introduction = introduction.trim();
+        if (null != lyric)
+            lyric = lyric.trim();
+        song.setName(name);
+        song.setIntroduction(introduction);
+        song.setLyric(lyric);
         return songMapper.insert(song) > 0;
     }
 
     @Override
     public boolean updateSong(Song song) {
+        // 去除首尾空格，trim不适用于null
+        String name = song.getName();
+        String introduction = song.getIntroduction();
+        String lyric = song.getLyric();
+        String pic = song.getPic();
+        String url = song.getUrl();
+        if (null != name)
+            name = name.trim();
+        if (null != introduction)
+            introduction = introduction.trim();
+        if (null != lyric)
+            lyric = lyric.trim();
+        // 如果pic或者url为空字符串，则统一设为null，不进行数据更新
+        if (StringUtils.isBlank(pic))
+            pic = null;
+        if (StringUtils.isBlank(url))
+            url = null;
+        song.setName(name);
+        song.setIntroduction(introduction);
+        song.setLyric(lyric);
+        song.setPic(pic);
+        song.setUrl(url);
         return songMapper.updateById(song) > 0;
     }
 
@@ -47,6 +83,12 @@ public class SongServiceImpl implements SongService {
 
     @Override
     public List<Song> getSongByName(String name) {
+        if (null != name)
+            name = name.trim();
+        // 在url携带参数会删除多余空格，但是body中不会，空白字符默认查询所有
+        if (StringUtils.isBlank(name))
+            name = "";
+        System.out.println(StringUtils.isBlank(name));
         LambdaQueryWrapper<Song> wrapper = new LambdaQueryWrapper<>();
         wrapper.like(Song::getName, name);
         return songMapper.selectList(wrapper);
@@ -54,6 +96,8 @@ public class SongServiceImpl implements SongService {
 
     @Override
     public Song getSongByNameEq(String name) {
+        if (StringUtils.isNotBlank(name))
+            name = name.trim();
         LambdaQueryWrapper<Song> wrapper = new LambdaQueryWrapper<>();
         wrapper.eq(Song::getName, name);
         return songMapper.selectOne(wrapper);
